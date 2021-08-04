51e809c3eb7ddfbe0cce8daf6372a159148da573
Cem Kurulay <cemkurulay@outlook.com>
2021-08-05 00:11:36 +0300

    Fix nonetype errors for questions

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import threading
 import queue
 from bs4 import BeautifulSoup
 from sitemap import SiteMapManager
-import datetime
 import yake
 
 
-yesNoText = ""      # global variabble to be used for yes/no questions
-class Menu:
+yesNoText = ""      # global variable to be used for searching the answer of yes/no questions in it
+
+class Menu:   #menu as a class that adds, removes and displays the options also called as items 
     def __init__(self, name, items=None):
-        self.name = name
-        self.items = items or []
+        self.name = name     #one option of the menu
+        self.items = items or []  #array of combined options
 
     def add_item(self, item):
         self.items.append(item)
@@ -33,8 +33,8 @@ class Menu:
             item.draw()
 
 class Item:
-    def __init__(self, name, function, parent=None):
-        self.name = name
+    def __init__(self, name, function, parent=None): 
+        self.name = name   # menu content (ex: 1.crawl a webpage 2.print sitemap ...)
         self.function = function
         self.parent = parent
         if parent:
@@ -47,6 +47,7 @@ class Crawler(threading.Thread):    # crawls all internal links by implementing 
 
     def __init__(self, base_url, links_to_crawl, visited_links, inaccessible_links, url_lock): #constructor
         threading.Thread.__init__(self)
+
         print(f"Web Crawler worker {threading.current_thread()} has Started")
 
         self.domain = urlparse(base_url).netloc
@@ -62,7 +63,8 @@ class Crawler(threading.Thread):    # crawls all internal links by implementing 
         }
         return requests.get(url, headers=headers).text
 
-    def get_linked_urls(self, url, html):
+    # extract outgoing urls from given link which are in the same domain
+    def get_linked_urls(self, url, html): 
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
             path = link.get('href')
@@ -70,9 +72,10 @@ class Crawler(threading.Thread):    # crawls all internal links by implementing 
                 path = urljoin(url, path)
             if path is None or path.startswith('#') or self.domain != urlparse(path).netloc:
                 continue
-            siteMapManager.add_url_connection(url, path)
+            siteMapManager.add_url_connection(url, path) 
             yield path
 
+    # extract static assets (img, link, script) from given webpage and store their url addresses
     def add_asset_links(self, url, html):
         soup = BeautifulSoup(html, 'html.parser')
         testVariable = soup.find_all(['img', 'script', 'link'])
@@ -87,10 +90,12 @@ class Crawler(threading.Thread):    # crawls all internal links by implementing 
                 continue
             siteMapManager.add_asset_connection(url, assetLink)
 
+    # add a new url to queue to visit
     def add_url_to_visit(self, url):
         if url not in self.visited_links and url not in self.inaccessible_links:
             self.links_to_crawl.put(url)
 
+    # call add asset links and add outgoing urls to queue functions for given url
     def crawl(self, url):
         html = self.download_url(url)
         self.add_asset_links(url, html)
@@ -98,6 +103,7 @@ class Crawler(threading.Thread):    # crawls all internal links by implementing 
         for url in self.get_linked_urls(url, html):
             self.add_url_to_visit(url)
 
+    # get url from queue thread safe, crawl that webpage, add it to visited urls
     def run(self):
         while self.links_to_crawl:
             self.url_lock.acquire()
@@ -118,12 +124,13 @@ class Crawler(threading.Thread):    # crawls all internal links by implementing 
                 self.links_to_crawl.task_done()
                 self.visited_links.add(url)
 
+# define global site map manager and question answerer classes
 siteMapManager = SiteMapManager()
 answerMap = {}
 yesNoMap = {}
 
 
-def crawlWebpage():
+def crawlWebpage(): #funtion where Crawler class is called
     url = input("Please enter a URL to crawl: ")
     
     number_of_threads = input("Please Enter number of Threads: ")
@@ -134,15 +141,15 @@ def crawlWebpage():
 
     getElements(url)
 
-    links_to_crawl = queue.Queue()
-    url_lock = threading.Lock()
-    links_to_crawl.put(url)
+    links_to_crawl = queue.Queue()  #stores links to be crawled
+    url_lock = threading.Lock()  # will be used to prevent threads from simultaneously accessing a shared data
+    links_to_crawl.put(url)  
 
-    visited_links = set()
+    visited_links = set()   
     crawler_threads = []
     inaccessible_links = set()
 
-    for i in range(int(number_of_threads)):
+    for i in range(int(number_of_threads)): #running the crawler with threads
         crawler = Crawler(base_url=url,
                         links_to_crawl=links_to_crawl,
                         visited_links=visited_links,
@@ -152,7 +159,7 @@ def crawlWebpage():
         crawler.start()
         crawler_threads.append(crawler)
 
-    for crawler in crawler_threads:
+    for crawler in crawler_threads: #ending all threads
         crawler.join()
 
     print(f"Total Number of pages visited are {len(visited_links)}")
@@ -162,7 +169,7 @@ def crawlWebpage():
 #assigns key-words to -answerMap- (used to answer WH questions)
 def key_allocation(arr, val):
     for i in arr:
-        answerMap[i] = val 
+        answerMap[i] = val.get_text() 
 
 def getElements(url):    #getting possible elements from the provided page (ex: price, contact info, and etc)
    
@@ -175,54 +182,64 @@ def getElements(url):    #getting possible elements from the provided page (ex: 
     s = BeautifulSoup(r.content, "html.parser")
     
     #extracting address
-    address = s.find('h1', class_='homeAddress-variant').get_text()
+    
+    address = s.find('h1', class_='homeAddress-variant')
     address_array = ['address', 'location', 'where', 'place', 'site']
-    key_allocation(address_array, address)
+    if address is not None:
+        key_allocation(address_array, address)
 
     #extracting price, beds, baths and size info
     info_div = s.find('div', class_= 'home-main-stats-variant')
-    
-    price = info_div.find('div', class_= 'stat-block price-section').get_text()
-    beds = info_div.find('div', class_= 'stat-block beds-section').get_text()
-    baths = info_div.find('div', class_= 'stat-block baths-section').get_text()
-    size = info_div.find('div', class_= 'stat-block sqft-section').get_text()
+    if info_div is not None:
+        price = info_div.find('div', class_= 'stat-block price-section')
+        beds = info_div.find('div', class_= 'stat-block beds-section')
+        baths = info_div.find('div', class_= 'stat-block baths-section')
+        size = info_div.find('div', class_= 'stat-block sqft-section')
 
-    price_array = ['price', 'cost', 'fee', 'payment', 'amount', 'worth', 'how much']
-    key_allocation(price_array, price[:-5])
+        price_array = ['price', 'cost', 'fee', 'payment', 'amount', 'worth', 'how much']
+        if price is not None:
+            for i in price_array:
+                answerMap[i] = price.get_text()[:-5] 
+            
+        if beds is not None:
+            answerMap['bed'] = beds.get_text()[:-4]+" beds"
+            answerMap['bath']=baths.get_text()[:-5]+" baths"
 
-    answerMap['bed'] = beds[:-4]+" beds"
-    answerMap['bath']=baths[:-5]+" baths"
-
-    size_array = ['size', 'dimension', 'width', 'capacity']
-    key_allocation(size_array, size[:-5] + " Sq Ft")
+        size_array = ['size', 'dimension', 'width', 'capacity']
+        if size is not None:
+            for i in size_array:
+                answerMap[i] = size.get_text()[:-5] + " Sq Ft"
+  
 
     #extracting info about owner
     agentInfo = s.find('div', class_='agent-info-item')
-    
-    owner = agentInfo.find('div', class_='agent-basic-details font-color-gray-dark')
-    owner_clean = owner.select("span a")[0].get_text()
-    
-    #extracting contact info
-    phoneElement = agentInfo.find('p', class_='phone-numbers')
-    if phoneElement is not None:
-        answerMap['phone'] = phoneElement.get_text()
-        answerMap['number'] = phoneElement.get_text()
-    
-    
-    emailElement = agentInfo.find('a', class_='phone-number-entry')
-    if emailElement is not None:
-        answerMap['email'] = emailElement.get_text()
+    if agentInfo is not None:
+        owner = agentInfo.find('div', class_='agent-basic-details font-color-gray-dark')
+        owner_clean = owner.select("span a")[0]
         
-    if phoneElement is not None and emailElement is not None:
-        answerMap['contact'] = "phone number "+phoneElement.get_text() + "  " + "email is "+emailElement.get_text()
-    
-    owner_array=['owner', 'agent', 'who']
-    key_allocation(owner_array, owner_clean)
-    
+        #extracting contact info
+        phoneElement = agentInfo.find('p', class_='phone-numbers')
+        if phoneElement is not None:
+            answerMap['phone'] = phoneElement.get_text()
+            answerMap['number'] = phoneElement.get_text()
+        
+        
+        emailElement = agentInfo.find('a', class_='phone-number-entry')
+        if emailElement is not None:
+            answerMap['email'] = emailElement.get_text()
+            
+        if phoneElement is not None and emailElement is not None:
+            answerMap['contact'] = "phone number "+phoneElement.get_text() + "  " + "email is "+emailElement.get_text()
+        
+        owner_array=['owner', 'agent', 'who']
+        key_allocation(owner_array, owner_clean)
+        
 
-    #text t
+    #text used to search for keywords to aswer yes/no questions
     global yesNoText
-    yesNoText = s.find('div', class_= 'amenities-container').get_text().lower()
+    yesNoContent = s.find('div', class_= 'amenities-container')
+    if yesNoContent is not None:
+        yesNoText = yesNoContent.get_text().lower()
     #print(yesNoText)
 
 def drawSitemap():
@@ -246,15 +263,26 @@ def answerWHQuestion():
         print('Sorry, I do not know the answer for that question. ')
 
 def answerYesNoQuestion():
-    yesNoQuestion = input("Please enter a yes/no question: ")
 
+    yesNoQuestion = input("Please enter a yes/no question: ")
+     
+    if len(yesNoText) < 1:
+        print('I could not answer it.')
+        return
+
+    # define configuration variables for yake library's custom keyword extractor 
     language = "en"
     max_ngram_size = 3
     deduplication_threshold = 0.9
     numOfKeywords = 5
+
+    # create the custom keyword extractor
     custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+    
+    # extract keywords from yes/no question
     keywords = custom_kw_extractor.extract_keywords(yesNoQuestion)
     
+    # answer yes if extracted keywords exist in yes/no text 
     isNo = True
     for kw in keywords:
         if kw[0] in yesNoText: 
@@ -266,6 +294,7 @@ def answerYesNoQuestion():
 
 def exitProgram():
     return
+
 
 main = Menu("mainMenu")
 
